05bbc0b1598cefb653c075ab408496f77759e1ea
Jordi Albert Ribas González <jordiialb14@gmail.com>
2022-03-12 09:05:38 +0100

    Exercise 1.4 - Make all objects part of one array

--- src/components/App.js
+++ src/components/App.js
@@ -4,36 +4,26 @@ import { Total } from "./Total"
 
 export const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-  const parts = {
-    part1: part1,
-    part2: part2,
-    part3: part3
-  }
-
-  const exercises = {
-    exercises1: part1.exercises,
-    exercises2: part2.exercises,
-    exercises3: part3.exercises
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total exercises={exercises}/>
+      <Total parts={parts}/>
     </div>
   )
 }
